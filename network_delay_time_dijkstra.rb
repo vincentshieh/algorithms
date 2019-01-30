b0ba0efc0faedf310e5814aa93f319757f5b7c3d
Vincent Shieh <vincent.shieh@gmail.com>
2019-01-29 16:08:32 -0800

    add problem statement for Network Delay Time

--- network_delay_time_dijkstra.rb
+++ network_delay_time_dijkstra.rb
@@ -1,36 +1,44 @@
-# @param {Integer[][]} times
-# @param {Integer} n
-# @param {Integer} k
-# @return {Integer}
+# There are N network nodes, labelled 1 to N.
+#
+# Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, and w is the time it takes for a signal to travel from source to target.
+#
+# Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+#
+# Note:
+# N will be in the range [1, 100].
+# K will be in the range [1, N].
+# The length of times will be in the range [1, 6000].
+# All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 1 <= w <= 100.
+
 module ALDiGraph
     class Vertex
         attr_accessor :val, :dist, :prev
-        
+
         def initialize(val)
             @val = val
             @dist = Float::INFINITY
             @prev = nil
         end
     end
-    
+
     class Graph
         attr_accessor :graph
-        
+
         def initialize
             @graph = {}
         end
-        
+
         def add_vertex(v)
             @graph[v] = {
                 vertex: ALDiGraph::Vertex.new(v),
                 adjacents: []
             } if @graph[v].nil?
         end
-        
+
         def num_vertices
             @graph.keys.length
         end
-        
+
         def remove_vertex(v)
             deleted = @graph.delete(v)
             @graph.each_value do |al|
@@ -38,23 +46,23 @@ module ALDiGraph
             end
             deleted
         end
-        
+
         def add_edge(v1, v2, w)
             return if @graph[v1].nil? || @graph[v2].nil?
             v1_adjacents = @graph[v1][:adjacents]
             e = v1_adjacents.find { |adj| adj[0] == v2 }
             e ? e[1] = w : v1_adjacents << [v2, w]
         end
-        
+
         def get_edge_value(v1, v2)
             @graph[v1][:adjacents].find { |adj| adj[0] == v2 }[1]
         end
-        
+
         def remove_edge(v1, v2)
             return if @graph[v1].nil? || @graph[v2].nil?
             @graph[v1][:adjacents].delete_if { |adj| adj[0] == v2 }
         end
-        
+
         def print
             p @graph
         end
@@ -68,27 +76,27 @@ class MinHeap
         @heap = nums
         heapify
     end
-    
+
     def empty?
         @heap.empty?
     end
-    
+
     def peek
         @heap[0]
     end
-    
+
     def insert(num)
         @heap << num
         sift_up
     end
-    
+
     def extract_min
         @heap[0], @heap[@heap.length - 1] = @heap[@heap.length - 1], @heap[0]
         val = @heap.pop
         sift_down
         val
     end
-    
+
     def heapify
         i = 0
         while i < @heap.length
@@ -105,9 +113,9 @@ class MinHeap
             i += 1
         end
     end
-    
+
     private
-    
+
     def sift_up
         i = @heap.length - 1
         while i > 0
@@ -120,7 +128,7 @@ class MinHeap
             end
         end
     end
-    
+
     def sift_down
         return if @heap.length < 2
         i = 0
@@ -146,6 +154,10 @@ def relax(graph, v1, v2)
     end
 end
 
+# @param {Integer[][]} times
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer}
 def network_delay_time(times, n, k)
     graph = ALDiGraph::Graph.new
     times.each do |t|
@@ -159,7 +171,7 @@ def network_delay_time(times, n, k)
         graph.graph[key][:vertex].dist = 0 if key == k
         unvisited.insert(graph.graph[key][:vertex])
     end
-    
+
     while !unvisited.empty?
         curr = unvisited.extract_min
         graph.graph[curr.val][:adjacents].each do |adj|
@@ -167,7 +179,7 @@ def network_delay_time(times, n, k)
         end
         unvisited.heapify
     end
-    
+
     max = 0
     graph.graph.each_key do |key|
         max = graph.graph[key][:vertex].dist if graph.graph[key][:vertex].dist > max
